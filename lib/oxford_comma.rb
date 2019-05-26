f51380b202d611003afa4ef4e92354c714951972
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 15:24:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  answer = ""
+  if array.size == 1
+    answer = array.join
+  elsif array.size == 2
+    answer = array.join(" and ")  
+  else
+    lastItem = array.pop
+    answer = array.join(", ") + ", and " + lastItem
+  end
+  answer
 end
